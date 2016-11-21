6703e8b79fa8736be5cd7eb9492dc563b75ccde5
guypaul2 <s.wellenreiter@orange.fr>
2016-11-21 16:29:22 +0100

    Update Joueur.php

--- modele/jeu/Joueur.php
+++ modele/jeu/Joueur.php
@@ -1,4 +1,10 @@
 <?php
+  /**Yanis OUAKRIM
+   * Simow WELLENREITER
+   * Group 2 
+   * Programmation web coté serveur (M3104) : Mini-Projet : Master Mind
+   */
+
 class Joueur{
   private $pseudo;
   //associe un pseudo à un joueur
