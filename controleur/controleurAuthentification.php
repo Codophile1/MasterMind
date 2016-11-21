cdc9cdfeb3ad5ed0be559b5f35ab46d2f4853971
guypaul2 <s.wellenreiter@orange.fr>
2016-11-21 16:29:43 +0100

    Update controleurAuthentification.php

--- controleur/controleurAuthentification.php
+++ controleur/controleurAuthentification.php
@@ -1,4 +1,10 @@
 <?php 
+  /**Yanis OUAKRIM
+   * Simow WELLENREITER
+   * Group 2 
+   * Programmation web coté serveur (M3104) : Mini-Projet : Master Mind
+   */
+
 require_once("../modele/bd.php");
 
 class ControleurAuthentification {
