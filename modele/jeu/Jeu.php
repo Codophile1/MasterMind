6775d2994b560dd7427bdf7bcef6f99e893c4d25
guypaul2 <s.wellenreiter@orange.fr>
2016-11-21 16:33:22 +0100

    Update Jeu.php

--- modele/jeu/Jeu.php
+++ modele/jeu/Jeu.php
@@ -1,10 +1,9 @@
 <?php
-  /**Yanis OUAKRIM
+ /** Yanis OUAKRIM
    * Simow WELLENREITER
    * Group 2 
    * Programmation web coté serveur (M3104) : Mini-Projet : Master Mind
-   */
-
+  */
 require_once "modele/jeu/Joueur.php";
 require_once "modele/jeu/PhaseDeJeu.php";
 require_once "modele/jeu/IntelligenceArtificielle.php";
