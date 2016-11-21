22bb7368872010302cff75d0322f3d980569a2f4
guypaul2 <s.wellenreiter@orange.fr>
2016-11-21 15:34:22 +0100

    Update Joueur.php

--- modele/jeu/Joueur.php
+++ modele/jeu/Joueur.php
@@ -1,11 +1,11 @@
 <?php
 class Joueur{
   private $pseudo;
-
+  //associe un pseudo à un joueur
   public function __construct($pseud){
     $this->pseudo=$pseud;
   }
-
+  //retourne le pseudo du joueur
   public function getPseudo(){
     return $this->pseudo;
   }
