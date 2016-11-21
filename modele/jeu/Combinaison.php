e3f0c6b36f6b91862aff0738459bcda256ece146
guypaul2 <s.wellenreiter@orange.fr>
2016-11-21 16:28:35 +0100

    Update Combinaison.php

--- modele/jeu/Combinaison.php
+++ modele/jeu/Combinaison.php
@@ -1,4 +1,10 @@
 <?php
+  /**Yanis OUAKRIM
+   * Simow WELLENREITER
+   * Group 2 
+   * Programmation web coté serveur (M3104) : Mini-Projet : Master Mind
+   */
+
 //La Classe combinaison permet de ne pas avoir à gérer les combinaison  directement avec des tableau et ainsi de toujours avoir quatres valeurs;
 class Combinaison{
   private $combinaison;
